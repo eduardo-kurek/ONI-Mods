d9df22f2e2a47deb8dca0e607b25b4971e26c047
Eduardo Kurek <eduardokurek2003@gmail.com>
2025-03-11 16:15:39 -0300

    First commit

---
+++ Test/Test.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using PeterHan.PLib.Core;
+
+namespace Test;
+
+public class Test : KMod.UserMod2 {
+	
+	public override void OnLoad(Harmony harmony){
+		base.OnLoad(harmony);
+		PUtil.InitLibrary();
+	}
+	
+}

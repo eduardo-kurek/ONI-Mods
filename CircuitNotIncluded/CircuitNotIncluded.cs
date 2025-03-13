dea70cb78ac73b1a4fd38d3f325c445e0096f2e7
Eduardo Kurek <eduardokurek2003@gmail.com>
2025-03-13 15:42:03 -0300

    Creating Circuit Not Included

---
+++ CircuitNotIncluded/CircuitNotIncluded.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using PeterHan.PLib.Core;
+
+namespace CircuitNotIncluded;
+
+public class CircuitNotIncluded : KMod.UserMod2 {
+	
+	public override void OnLoad(Harmony harmony){
+		base.OnLoad(harmony);
+		PUtil.InitLibrary();
+	}
+	
+}

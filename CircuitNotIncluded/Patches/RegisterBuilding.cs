523e9c024955a078396faa9da7da9f7fbe0b803c
Eduardo Kurek <eduardokurek2003@gmail.com>
2026-01-14 22:31:48 -0300

    Add up to 5x5 circuits

--- CircuitNotIncluded/Patches/RegisterBuilding.cs
+++ CircuitNotIncluded/Patches/RegisterBuilding.cs
@@ -7,5 +7,21 @@ namespace CircuitNotIncluded.Patches;
 public class RegisterBuilding {
 	public static void Postfix(){
 		CircuitManager.Instance.RegisterCircuit(1, 3);
+		CircuitManager.Instance.RegisterCircuit(1, 4);
+		CircuitManager.Instance.RegisterCircuit(1, 5);
+		
+		CircuitManager.Instance.RegisterCircuit(2, 2);
+		CircuitManager.Instance.RegisterCircuit(2, 3);
+		CircuitManager.Instance.RegisterCircuit(2, 4);
+		CircuitManager.Instance.RegisterCircuit(2, 5);
+		
+		CircuitManager.Instance.RegisterCircuit(3, 3);
+		CircuitManager.Instance.RegisterCircuit(3, 4);
+		CircuitManager.Instance.RegisterCircuit(3, 5);
+		
+		CircuitManager.Instance.RegisterCircuit(4, 4);
+		CircuitManager.Instance.RegisterCircuit(4, 5);
+		
+		CircuitManager.Instance.RegisterCircuit(5, 5);
 	}
 }

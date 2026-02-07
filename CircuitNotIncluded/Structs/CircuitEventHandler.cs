a92139160954f37146d44b49b871d05ea5997197
Eduardo Kurek <eduardokurek2003@gmail.com>
2026-02-06 21:37:18 -0300

    Create base classe CircuitPort

--- CircuitNotIncluded/Structs/CircuitEventHandler.cs
+++ CircuitNotIncluded/Structs/CircuitEventHandler.cs
@@ -2,39 +2,20 @@ using UnityEngine;
 
 namespace CircuitNotIncluded.Structs;
 
-public class CircuitEventHandler : ILogicEventReceiver, ILogicUIElement {
-	private Circuit circuit;
-	private InputPort inputPort;
-	private int cell;
-	
-	public CircuitEventHandler(Circuit circuit, InputPort inputPort){
-		this.circuit = circuit;
-		this.inputPort = inputPort;
-		cell = circuit.GetActualCell(inputPort.Offset);
-	}
+public class CircuitEventHandler(Circuit parent, InputPort inputPort)
+	: CircuitPort(parent, inputPort), ILogicEventReceiver
+{
+	private InputPort inputPort => (InputPort)port;
 	
 	public void OnLogicNetworkConnectionChanged(bool connected){ }
 	
 	public void ReceiveLogicEvent(int value){
-		circuit.OnInputPortChanged(inputPort.OriginalId, value);
+		parent.OnInputPortChanged(inputPort.OriginalId, value);
 	}
 	
 	public int GetLogicCell(){
 		return cell;
 	}
-
-	public void Connect(){
-		Game.Instance.logicCircuitSystem.AddToNetworks(cell, this, true);
-		Game.Instance.logicCircuitManager.AddVisElem(this);
-	}
-
-	public void Disconnect(){
-		Game.Instance.logicCircuitSystem.RemoveFromNetworks(cell, this, true);
-		Game.Instance.logicCircuitManager.RemoveVisElem(this);
-	}
-
-	public Vector2 PosMin() => Grid.CellToPos2D(cell);
-	public Vector2 PosMax() => Grid.CellToPos2D(cell);
-	public int GetLogicUICell() => cell;
-	public LogicPortSpriteType GetLogicPortSpriteType() => LogicPortSpriteType.Input;
+	
+	public override LogicPortSpriteType GetLogicPortSpriteType() => LogicPortSpriteType.Input;
 }
--- CircuitNotIncluded/Structs/CircuitEventSender.cs
+++ CircuitNotIncluded/Structs/CircuitEventSender.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 namespace CircuitNotIncluded.Structs;
 
-public class CircuitEventSender : ILogicEventSender, ILogicUIElement {
-	private int cell;
+public class CircuitEventSender(Circuit circuit, OutputPort outputPort)
+	: CircuitPort(circuit, outputPort), ILogicEventSender
+{
+	private OutputPort outputPort => (OutputPort)port;
 	private int logicValue = 0;
-	public OutputPort OutputPort { get; }
-
-	public CircuitEventSender(Circuit circuit, OutputPort outputPort){
-		OutputPort = outputPort;
-		cell = circuit.GetActualCell(outputPort.Offset);
-	}
-
+	
 	public void OnLogicNetworkConnectionChanged(bool connected){ }
 
 	public void LogicTick(){
@@ -19,24 +15,12 @@ public class CircuitEventSender : ILogicEventSender, ILogicUIElement {
 	}
 
 	public void Refresh(SymbolTable symbolTable){
-		logicValue = OutputPort.Evaluate(symbolTable);
-	}
-	
-	public void Connect(){
-		Game.Instance.logicCircuitSystem.AddToNetworks(cell, this, true);
-		Game.Instance.logicCircuitManager.AddVisElem(this);
-	}
-
-	public void Disconnect(){
-		Game.Instance.logicCircuitSystem.RemoveFromNetworks(cell, this, true);
-		Game.Instance.logicCircuitManager.RemoveVisElem(this);
+		logicValue = outputPort.Evaluate(symbolTable);
 	}
 	
 	public int GetLogicCell() => cell;
 	public int GetLogicValue() => logicValue;
 	
-	public Vector2 PosMin() => Grid.CellToPos2D(cell);
-	public Vector2 PosMax() => Grid.CellToPos2D(cell);
-	public int GetLogicUICell() => cell;
-	public LogicPortSpriteType GetLogicPortSpriteType() => LogicPortSpriteType.Output;
+	public override LogicPortSpriteType GetLogicPortSpriteType() 
+		=> LogicPortSpriteType.Output;
 }
